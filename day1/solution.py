27ac8592484f0dc7eee84773c1e3a24653786324
Ken Crowell <oeuftete@gmail.com>
2018-12-01 14:13:55 -0400

    solution for day 1, second problem

--- day1/solution.py
+++ day1/solution.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from itertools import cycle
 import os
 
 
@@ -9,12 +10,25 @@ def read_input(file_name):
     return list(content)
 
 
-def frequency_summer(frequency_list):
-    return reduce(lambda x, y: x + y, map(int, frequency_list))
+def frequency_summer(freq_deltas):
+    return reduce(lambda x, y: x + y, map(int, freq_deltas))
+
+
+def frequency_repeat_checker(freq_deltas):
+    current_frequency = 0
+    frequencies_seen = set([current_frequency])
+    for x in cycle(map(int, freq_deltas)):
+        current_frequency += x
+        if current_frequency in frequencies_seen:
+            return current_frequency
+        frequencies_seen.add(current_frequency)
 
 
 if __name__ == '__main__':
-    print(frequency_summer(read_input(
-        os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                     '..', 'input/input-day1-1.txt')
-    )))
+    INPUT_DATA_FILE = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        '..', 'input/input-day1-1.txt'
+    )
+    frequencies = read_input(INPUT_DATA_FILE)
+    print("Problem 1:", frequency_summer(frequencies))
+    print("Problem 2:", frequency_repeat_checker(frequencies))
--- day1/tests/test_day1.py
+++ day1/tests/test_day1.py
@@ -1,7 +1,11 @@
 import os
 import pytest
 
-from day1.solution import frequency_summer, read_input
+from day1.solution import (
+    frequency_repeat_checker,
+    frequency_summer,
+    read_input,
+)
 
 
 def test_read_input():
@@ -11,10 +15,20 @@ def test_read_input():
     ) == ['-1', '+1', '-2']
 
 
-@pytest.mark.parametrize("frequencies,expected", [
+@pytest.mark.parametrize("freq_deltas,expected", [
     (['+1', '+1', '+1'], 3),
     (['+1', '+1', '-2'], 0),
     (['-1', '-2', '-3'], -6)
 ])
-def test_day1_1(frequencies, expected):
-    assert frequency_summer(frequencies) == expected
+def test_day1_1(freq_deltas, expected):
+    assert frequency_summer(freq_deltas) == expected
+
+
+@pytest.mark.parametrize("freq_deltas,expected", [
+    (['+1', '-1'], 0),
+    (['+3', '+3', '+4', '-2', '-4'], 10),
+    (['-6', '+3', '+8', '+5', '-6'], 5),
+    (['+7', '+7', '-2', '-7', '-4'], 14),
+])
+def test_day1_2(freq_deltas, expected):
+    assert frequency_repeat_checker(freq_deltas) == expected
