70c2ea059cc3bdd05b67496604446ee3e978c0cd
Ken Crowell <oeuftete@gmail.com>
2018-12-05 07:01:25 -0400

    Add day 5, part 2 solution

--- adventofcode/day5/solution.py
+++ adventofcode/day5/solution.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import string
 
 from adventofcode.common.helpers import read_input
 
@@ -23,6 +24,22 @@ def reacted_polymer(polymer):
     return reacted
 
 
+def reduced_reacted_polymer(polymer, bad_unit):
+    return reacted_polymer(polymer
+                           .replace(bad_unit, '')
+                           .replace(bad_unit.swapcase(), ''))
+
+
+def shortest_reduced_polymer(polymer, bad_units):
+    min_length = len(polymer)
+    for c in bad_units:
+        reacted_length = len(reduced_reacted_polymer(polymer, c))
+        if reacted_length < min_length:
+            min_length = reacted_length
+
+    return min_length
+
+
 if __name__ == '__main__':
     INPUT_DATA_FILE = os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
@@ -30,4 +47,5 @@ if __name__ == '__main__':
     )
     polymer = read_input(INPUT_DATA_FILE)[0]
     print("Problem 1:", len(reacted_polymer(polymer)))
-    print("Problem 2:", 'TODO')
+    print("Problem 2:", shortest_reduced_polymer(polymer,
+                                                 string.ascii_lowercase))
--- adventofcode/day5/tests/test_solution.py
+++ adventofcode/day5/tests/test_solution.py
@@ -1,12 +1,34 @@
 import pytest
 
 from adventofcode.day5.solution import (
-    reacted_polymer
+    reacted_polymer,
+    reduced_reacted_polymer,
+    shortest_reduced_polymer
 )
 
 
+TEST_POLYMER = 'dabAcCaCBAcCcaDA'
+
+
 @pytest.mark.parametrize("polymer,reacted", [
-    ('dabAcCaCBAcCcaDA', 'dabCBAcaDA'),
+    (TEST_POLYMER, 'dabCBAcaDA'),
 ])
 def test_reacted_polymer(polymer, reacted):
     assert reacted_polymer(polymer) == reacted
+
+
+@pytest.mark.parametrize("polymer,bad_unit,reacted_length", [
+    (TEST_POLYMER, 'a', 6),
+    (TEST_POLYMER, 'b', 8),
+    (TEST_POLYMER, 'c', 4),
+    (TEST_POLYMER, 'd', 6),
+])
+def test_reduced_reacted_polymer(polymer, bad_unit, reacted_length):
+    assert len(reduced_reacted_polymer(polymer, bad_unit)) == reacted_length
+
+
+@pytest.mark.parametrize("polymer,bad_units,reacted_length", [
+    (TEST_POLYMER, 'abcd', 4),
+])
+def test_shortest_reduced_polymer(polymer, bad_units, reacted_length):
+    assert shortest_reduced_polymer(polymer, bad_units) == reacted_length
