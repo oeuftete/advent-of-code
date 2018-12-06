231f36be03824a7cb5abff5d0ba574af77608acb
Ken Crowell <oeuftete@gmail.com>
2018-12-06 08:13:23 -0400

    Add day 6, part 1 solution

---
+++ adventofcode/day6/solution.py
@@ -0,0 +1,179 @@
+from collections import defaultdict, Counter
+from functools import total_ordering
+import logging
+import math
+
+from aocd import get_data
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+@total_ordering
+class Coordinate():
+    def __init__(self, **kwargs):
+        if kwargs.get('csv', None):
+            (self.x, self.y) = map(int,
+                                   kwargs['csv'].replace(" ", "").split(","))
+        else:
+            self.x = kwargs['x']
+            self.y = kwargs['y']
+
+    def manhattan_distance(self, other):
+        return abs(self.x - other.x) + abs(self.y - other.y)
+
+    def __eq__(self, other):
+        return ((self.x, self.y) == (other.x, other.y))
+
+    def __lt__(self, other):
+        return ((self.x, self.y) < (other.x, other.y))
+
+    def __str__(self):
+        return "({}, {})".format(self.x, self.y)
+
+    def is_bounded_by(self, other, direction):
+        if direction == 'east':
+            return (self.x < other.x
+                    and (other.x - self.x) >= abs(self.y - other.y))
+        elif direction == 'west':
+            return (self.x > other.x
+                    and (self.x - other.x) >= abs(self.y - other.y))
+        elif direction == 'north':
+            return (self.y < other.y
+                    and (other.y - self.y) >= abs(self.x - other.x))
+        elif direction == 'south':
+            return (self.y > other.y
+                    and (self.y - other.y) >= abs(self.x - other.x))
+
+        raise ValueError
+
+
+def get_boundaries(coordinates):
+    by_x = sorted(coordinates, key=lambda c: c.x)
+    by_y = sorted(coordinates, key=lambda c: c.y)
+    return ((by_x[0].x, by_x[-1].x),
+            (by_y[0].y, by_y[-1].y))
+
+
+def get_bound_indices(coordinates):
+    unbound = list()
+    i = 0
+
+    for c in coordinates:
+        bounded = defaultdict(bool)
+        for c_other in coordinates:
+            logging.debug('Checking {} against {}...'.format(c, c_other))
+            if c == c_other:
+                logging.debug('...identity: {} == {}'.format(c, c_other))
+                continue
+
+            for d in ['west', 'east', 'north', 'south']:
+                if c.is_bounded_by(c_other, d):
+                    logging.debug('...{} bound to the {} by {}'
+                                  .format(c, d, c_other))
+                    bounded[d] = True
+                    continue
+
+        if (bounded['west'] and bounded['east']
+                and bounded['north'] and bounded['south']):
+            logging.debug('{} ({}) bound in all directions'.format(c, i))
+            unbound.append(i)
+        else:
+            logging.debug('{} ({}) not bound: {}'.format(c, i, bounded))
+
+        i += 1
+
+    return unbound
+
+
+def get_boundary_indices(coordinates):
+    ((min_x, max_x), (min_y, max_y)) = get_boundaries(coordinates)
+    found = list()
+
+    i = 0
+    for c in coordinates:
+        if c.x == min_x or c.x == max_x or c.y == min_y or c.y == max_y:
+            found.append(i)
+        i += 1
+
+    return found
+
+
+def largest_area(coordinates):
+    ((min_x, max_x), (min_y, max_y)) = get_boundaries(coordinates)
+    range_x = max_x - min_x
+    range_y = max_y - min_y
+
+    winners = Counter()
+
+    #  For each point within the boundary...
+    for x in range(min_x - int(range_x/2), max_x + int(range_x/2)):
+        for y in range(min_y - int(range_y/2), max_y + int(range_y/2)):
+
+            logging.debug('Checking {}:{}...'.format(x, y))
+
+            target = Coordinate(x=x, y=y)
+            min_length = math.inf
+            leading_i = None
+
+            i = 0
+            for c in coordinates:
+                distance = c.manhattan_distance(target)
+                if distance == 0:
+                    logging.debug('C {} {} won {}:{} (identity)!'
+                                  .format(i, c, x, y))
+                    winners.update([i])
+                    leading_i = None
+                    break
+
+                elif distance == min_length:
+                    logging.debug('{}:{} was pushed by C {} {}.'
+                                  .format(x, y, i, c))
+                    leading_i = None
+
+                elif distance < min_length:
+                    logging.debug('C {} {} took the lead at {}:{}...'
+                                  .format(i, c, x, y))
+                    leading_i = i
+                    min_length = distance
+
+                i += 1
+
+            if leading_i is not None:
+                logging.debug('C {} won {}:{} (after full search)!'
+                              .format(leading_i, x, y))
+                winners.update([leading_i])
+
+    logging.debug(winners)
+
+    bound_indices = get_bound_indices(coordinates)
+
+    for w in winners.most_common():
+        (winner_index, winner_count) = w
+
+        logging.debug("Checking winner {}".format(winner_index))
+        if winner_index in bound_indices:
+            logging.debug("Leader {} ({}) is not bound.  Wins!"
+                          .format(winner_index, winner_count))
+            return winner_count
+
+        logging.debug("Leader {} ({}) was unbound..."
+                      .format(winner_index, winner_count))
+
+    #    For each coordinate...
+    #      Find it's distance to the target point...
+    #         If it's:
+    #           0: this coordinate wins this one, move on
+    #           non-zero:
+    #             is it equal to the current min?  If so, draw.  Move on
+    #             is it lt the current min?  New leader, continue search
+    #             Otherwise, implicitly gt, continue search
+    #
+    #  See who won the most, return the count of it's wins
+
+
+if __name__ == '__main__':
+    coordinates = list(map(lambda c: Coordinate(csv=c),
+                           get_data(year=2018, day=6).split('\n')))
+    print("Problem 1:", largest_area(coordinates))
+    print("Problem 2:", 'TBD')
