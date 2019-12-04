e4693e1e6879ed0b5af451ac8174b4da22933b1a
Ken Crowell <oeuftete@gmail.com>
2019-12-03 22:07:15 -0400

    Add day1 solution, part b

--- adventofcode/year2019/day1/solution.py
+++ adventofcode/year2019/day1/solution.py
@@ -1,17 +1,32 @@
 from aocd.models import Puzzle
 
 
+def recursive_fuel_requirement(m):
+    """Return the fuel requirement for one mass and its fuel."""
+    if m <= 0:
+        return 0
+
+    new_fuel = fuel_requirement(m)
+    return new_fuel + recursive_fuel_requirement(new_fuel)
+
+
 def fuel_requirement(m):
     """Return the fuel requirement for one mass."""
-    return int(m / 3) - 2
+    return max(0, int(m / 3) - 2)
 
 
-def fuel_requirements(masses):
+def fuel_requirements(masses, include_fuel=False):
     """Return the fuel requirements for a list of masses."""
-    return sum(map(fuel_requirement, masses))
+    if include_fuel:
+        requirement_f = recursive_fuel_requirement
+    else:
+        requirement_f = fuel_requirement
+
+    return sum(map(requirement_f, masses))
 
 
 if __name__ == '__main__':
     puzzle = Puzzle(year=2019, day=1)
-    masses = map(int, puzzle.input_data.split('\n'))
+    masses = list(map(int, puzzle.input_data.split('\n')))
     puzzle.answer_a = fuel_requirements(masses)
+    puzzle.answer_b = fuel_requirements(masses, include_fuel=True)
--- adventofcode/year2019/day1/tests/test_solution.py
+++ adventofcode/year2019/day1/tests/test_solution.py
@@ -1,7 +1,8 @@
 import pytest
 
 from adventofcode.year2019.day1.solution import (fuel_requirement,
-                                                 fuel_requirements)
+                                                 fuel_requirements,
+                                                 recursive_fuel_requirement)
 
 
 @pytest.mark.parametrize("m,f", [
@@ -14,8 +15,18 @@ def test_fuel_requirement(m, f):
     assert fuel_requirement(m) == f
 
 
-@pytest.mark.parametrize("masses, f", [
-    ([12, 14, 1969, 100756], 2 + 2 + 654 + 33583),
+@pytest.mark.parametrize("m,f", [
+    (14, 2),
+    (1969, 966),
+    (100756, 50346),
 ])
-def test_fuel_requirements(masses, f):
-    assert fuel_requirements(masses) == f
+def test_recursive_fuel_requirement(m, f):
+    assert recursive_fuel_requirement(m) == f
+
+
+@pytest.mark.parametrize("masses, f, include_fuel", [
+    ([12, 14, 1969, 100756], 2 + 2 + 654 + 33583, False),
+    ([14, 1969, 100756], 2 + 966 + 50346, True),
+])
+def test_fuel_requirements(masses, f, include_fuel):
+    assert fuel_requirements(masses, include_fuel=include_fuel) == f
