3dfe11d933540d3e67b6f841ae67a50761960637
Ken Crowell <oeuftete@gmail.com>
2019-12-05 17:53:22 -0400

    Use an instruction pointer for Intcode

--- adventofcode/common/year2019/intcode_computer.py
+++ adventofcode/common/year2019/intcode_computer.py
@@ -17,6 +17,8 @@ class Intcode(object):
         if self.verb:
             self.opcodes[2] = self.verb
 
+        self.pointer = 0
+
         self.input_data = input_data or list()
         self.output_data = list()
 
@@ -30,14 +32,10 @@ class Intcode(object):
         return op, modes
 
     def execute(self):
-        move_forward = 0
         opcodes = self.opcodes
 
-        for i, op in enumerate(opcodes):
-
-            if move_forward > 0:
-                move_forward = move_forward - 1
-                continue
+        while True:
+            i = self.pointer
 
             op, modes = self.parse_op(opcodes[i])
 
@@ -49,7 +47,7 @@ class Intcode(object):
                     opcodes[i + 3] = s
                 else:
                     opcodes[opcodes[i + 3]] = s
-                move_forward = 3
+                self.pointer += 4
             #  MULTIPLY
             elif op == 2:
                 m = opcodes[i + 1] if modes[0] else opcodes[opcodes[i + 1]]
@@ -58,14 +56,14 @@ class Intcode(object):
                     opcodes[i + 3] = m
                 else:
                     opcodes[opcodes[i + 3]] = m
-                move_forward = 3
+                self.pointer += 4
             #  INPUT
             elif op == 3:
                 if modes[0]:
                     opcodes[i + 1] = self.input_data.pop(0)
                 else:
                     opcodes[opcodes[i + 1]] = self.input_data.pop(0)
-                move_forward = 1
+                self.pointer += 2
             #  OUTPUT
             elif op == 4:
                 if modes[0]:
@@ -75,7 +73,7 @@ class Intcode(object):
 
                 self.output_data.append(o)
                 logging.debug(f'Output data appended: {o}')
-                move_forward = 1
+                self.pointer += 2
             #  HALT
             elif op == 99:
                 return opcodes
