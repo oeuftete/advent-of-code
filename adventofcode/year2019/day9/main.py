4fc2a627a8294f29c5299b36c986984c67464d67
Ken Crowell <oeuftete@gmail.com>
2019-12-09 21:34:21 -0400

    Add solution for 2019 day 9 b

--- adventofcode/year2019/day9/main.py
+++ adventofcode/year2019/day9/main.py
@@ -8,6 +8,10 @@ if __name__ == '__main__':
     puzzle = Puzzle(year=2019, day=9)
     opcodes = puzzle.input_data
 
-    intcode = Intcode(opcodes, input_data=[1])
-    intcode.execute()
-    puzzle.answer_a = intcode.last_output
+    intcode_test_mode = Intcode(opcodes, input_data=[1])
+    intcode_test_mode.execute()
+    puzzle.answer_a = intcode_test_mode.last_output
+
+    intcode_boost_mode = Intcode(opcodes, input_data=[2])
+    intcode_boost_mode.execute()
+    puzzle.answer_b = intcode_boost_mode.last_output
