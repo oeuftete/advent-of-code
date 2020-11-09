77c8a5d7034a689b7c73c3dab0dcd4d17c1aa567
Ken Crowell <oeuftete@gmail.com>
2020-11-08 23:18:01 -0400

    wip: working for first two examples

--- adventofcode/year2019/day14/solution.py
+++ adventofcode/year2019/day14/solution.py
@@ -1,9 +1,9 @@
-#  import logging
+import logging
 import math
 import re
 
-from cached_property import cached_property
 import networkx as nx
+from cached_property import cached_property
 
 
 class Nanofactory(object):
@@ -15,9 +15,14 @@ class Nanofactory(object):
             input_reactions = input_reactions.split("\n")
         self.input_reactions = input_reactions
 
+        self.ore_requests = []
+
         self._build_graph()
 
     def _build_graph(self):
+        #  We have as much ORE as we need
+        self.g.add_node("ORE", reserve=math.inf)
+
         REACTION_FORMAT = re.compile(r"(.*?) => (.*)")
         REACTANT_FORMAT = re.compile(r"(\d+) (\w+)")
         for reaction in self.input_reactions:
@@ -27,8 +32,7 @@ class Nanofactory(object):
             m_output = re.match(REACTANT_FORMAT, output)
             n_output, output_reactant = m_output.groups()
 
-            #  On the node, n is the number of output reactants generated
-            self.g.add_node(output_reactant, n=n_output, reserves=0)
+            self.g.add_node(output_reactant, reserve=0)
 
             for input_string in [s.strip() for s in inputs.split(",")]:
                 m_input = re.match(REACTANT_FORMAT, input_string)
@@ -36,15 +40,93 @@ class Nanofactory(object):
                 #  On the edge, n is the number of input reactants required,
                 #  and cost is the total cost.
                 self.g.add_edge(
-                    output_reactant, input_reactant, n=n_output, cost=n_input
+                    output_reactant,
+                    input_reactant,
+                    n=int(n_output),
+                    cost=int(n_input),
                 )
 
-            #  We have as much ORE as we need
-            self.g.add_node("ORE", reserves=math.inf)
+    def reset(self):
+        for n in self.g.nodes():
+            self.g.nodes[n]["reserve"] = 0
+
+    def minimum_ore_for_chemical(self, chemical="FUEL", n=1, top_level=True):
+        # Start at `chemical`
+        #
+        # Figure out the cost in immediate precursors.
+        # For each precursor that's not ORE, recursively call this f
+        # If it's ORE, add an item in ore withdrawal, don't add these together.
+        #
+        # E.g. for the example with FUEL:
+        #   - Add costs: 1E, 7A
+        #     - Add costs: 1D, 7A
+        #       - Add costs: 1C, 7A
+        #         - Add costs: 1B, 7A
+        #           - Add costs: 1 ORE (now [1B])
+        #           - Add costs: 7 ORE (now [1B, 7A])
+        #         - Add costs: 7 ORE (now [1B, 7A, 7A])
+        #       - Add costs: 7 ORE (now [1B, 7A, 7A, 7A])
+        #     - Add costs: 7 ORE (now [1B, 7A, 7A, 7A, 7A])
+
+        logging.debug(f"Determining ORE requirement for {n} {chemical}")
+        logging.debug(self.g.edges([chemical], data=True))
+        for _, precursor, reaction_data in self.g.edges([chemical], data=True):
+            logging.debug(f"... precursor found: {precursor}: {reaction_data}")
+            if precursor == "ORE":
+                logging.debug(f"Adding ORE request for {n} {chemical}.")
+                self.ore_requests.append(
+                    (chemical, n, reaction_data["cost"], reaction_data["n"])
+                )
+            else:
+                cost = reaction_data["cost"]
+                rx_n = reaction_data["n"]
+                for _ in range(cost):
+                    logging.debug(f"Making recursive request for {rx_n} {precursor}.")
+                    self.minimum_ore_for_chemical(precursor, rx_n, top_level=False)
+
+        if not top_level:
+            return
+
+        self.ore_requests.sort(key=lambda x: x[3])
+        logging.debug(f"ORE request for {n} {chemical}: {self.ore_requests}")
+
+        # Now withdraw from ORE reserve, largest ORE withdrawal first
+        #
+        # check for A:
+        #   not enough, react:
+        #     A: output +10: (now [1B, 7A, 7A, 7A, 7A])
+        #     A: reserve = 3 (now [1B, 7A, 7A, 7A])
+        # check for A:
+        #   not enough, react:
+        #     A: output +10: (now [1B, 7A, 7A, 7A])
+        #     A: reserve = 6 (now [1B, 7A, 7A])
+        # check for A:
+        #   not enough, react:
+        #     A: output +10: (now [1B, 7A, 7A])
+        #     A: reserve = 9 (now [1B, 7A])
+        # check for A:
+        #   enough:
+        #     A: output +10: (now [1B, 7A])
+        #     A: reserve = 2 (now [1B])
+        # check for B:
+        #   not enough, react:
+        #     A: output +10: (now [1B, 7A, 7A])
+        #     A: reserve = 9 (now [1B, 7A])
+
+        ore_spent = 0
+
+        while self.ore_requests:
+            (chemical, n, cost, n_created) = self.ore_requests.pop()
+            #  Get the current chemical reserve
+            #  If it's not enough, react until there are enough
+            while self.g.nodes[chemical]["reserve"] < n:
+                ore_spent += cost
+                self.g.nodes[chemical]["reserve"] += n_created
+
+            self.g.nodes[chemical]["reserve"] -= n
+
+        return ore_spent
 
     @cached_property
     def minimum_ore(self):
-        for p in nx.all_simple_paths(self.g, "FUEL", "ORE"):
-            #  Traversing the edges this way isn't what's required.  Need to
-            #  figure it out.
-            pass
+        return self.minimum_ore_for_chemical("FUEL", 1)
--- adventofcode/year2019/day14/tests/test_solution.py
+++ adventofcode/year2019/day14/tests/test_solution.py
@@ -2,20 +2,22 @@ import pytest
 
 from adventofcode.year2019.day14.solution import Nanofactory
 
-TEST_CASES = [
-    (
-        """
+
+@pytest.fixture
+def reaction_one():
+    return """
 10 ORE => 10 A
 1 ORE => 1 B
 7 A, 1 B => 1 C
 7 A, 1 C => 1 D
 7 A, 1 D => 1 E
 7 A, 1 E => 1 FUEL
-""".strip(),
-        31,
-    ),
-    (
-        """
+""".strip()
+
+
+@pytest.fixture
+def reaction_two():
+    return """
 9 ORE => 2 A
 8 ORE => 3 B
 7 ORE => 5 C
@@ -23,13 +25,34 @@ TEST_CASES = [
 5 B, 7 C => 1 BC
 4 C, 1 A => 1 CA
 2 AB, 3 BC, 4 CA => 1 FUEL
-""".strip(),
-        165,
-    ),
-]
+""".strip()
 
 
-@pytest.mark.parametrize("input_data,minimum_ore", TEST_CASES)
-def test_factory(input_data, minimum_ore):
-    factory = Nanofactory(input_data)
-    assert factory.minimum_ore == minimum_ore
+def test_reaction_one(reaction_one):
+    factory = Nanofactory(reaction_one)
+
+    assert len(factory.g.edges()) == 10
+
+    assert factory.minimum_ore_for_chemical("A", 1) == 10
+    factory.reset()
+
+    assert factory.minimum_ore_for_chemical("A", 1) == 10
+    factory.reset()
+
+    assert factory.minimum_ore_for_chemical("A", 11) == 20
+    factory.reset()
+
+    assert factory.minimum_ore == 31
+
+
+def test_reaction_two(reaction_two):
+    factory = Nanofactory(reaction_two)
+
+    #  assert factory.minimum_ore_for_chemical("A", 1) == 9
+    #  factory.reset()
+    #  assert factory.minimum_ore_for_chemical("A", 2) == 9
+    #  factory.reset()
+    #  assert factory.minimum_ore_for_chemical("A", 3) == 18
+    #  factory.reset()
+
+    assert factory.minimum_ore == 165
