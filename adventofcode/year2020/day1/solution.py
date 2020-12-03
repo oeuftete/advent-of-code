e1b9c22a3f67c3119c175bcd1f186931e71d6edd
Ken Crowell <oeuftete@gmail.com>
2020-12-02 21:57:20 -0400

    Add solution for 2020 day 1 b

--- adventofcode/year2020/day1/solution.py
+++ adventofcode/year2020/day1/solution.py
@@ -1,3 +1,5 @@
+import operator
+from functools import reduce
 from itertools import permutations
 
 from aocd.models import Puzzle
@@ -5,18 +7,20 @@ from cached_property import cached_property
 
 
 class ExpenseReport:
-    def __init__(self, expenses):
+    def __init__(self, expenses, tuple_size=2):
         self.expenses = expenses
+        self.tuple_size = tuple_size
 
     @cached_property
     def product_2020(self):
         """Return the product of two expenses that sum to 2020."""
-        for p in permutations(self.expenses, r=2):
-            if p[0] + p[1] == 2020:
-                return p[0] * p[1]
+        for p in permutations(self.expenses, r=self.tuple_size):
+            if reduce(operator.add, p) == 2020:
+                return reduce(operator.mul, p)
 
 
 if __name__ == "__main__":
     puzzle = Puzzle(year=2020, day=1)
     expenses = [int(m) for m in puzzle.input_data.split("\n")]
     puzzle.answer_a = ExpenseReport(expenses).product_2020
+    puzzle.answer_b = ExpenseReport(expenses, tuple_size=3).product_2020
--- adventofcode/year2020/day1/tests/test_solution.py
+++ adventofcode/year2020/day1/tests/test_solution.py
@@ -4,9 +4,10 @@ from adventofcode.year2020.day1.solution import ExpenseReport
 
 
 @pytest.fixture
-def simple_expense_report():
-    return ExpenseReport([1721, 979, 366, 299, 675, 1456])
+def simple_expenses():
+    return [1721, 979, 366, 299, 675, 1456]
 
 
-def test_product_2020(simple_expense_report):
-    assert simple_expense_report.product_2020 == 514579
+@pytest.mark.parametrize("tuple_size,product", [(2, 514579), (3, 241861950)])
+def test_product_2020(simple_expenses, tuple_size, product):
+    assert ExpenseReport(simple_expenses, tuple_size=tuple_size).product_2020 == product
