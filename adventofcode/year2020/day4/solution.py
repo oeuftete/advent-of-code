b72d63564fd7b359c7a8d84b7294ca3a56f86bcb
Ken Crowell <oeuftete@gmail.com>
2020-12-04 22:41:07 -0400

    Add solution for 2020 day 4 b

--- adventofcode/year2020/day4/solution.py
+++ adventofcode/year2020/day4/solution.py
@@ -1,4 +1,6 @@
 import logging
+import re
+from functools import partial
 
 from aocd.models import Puzzle
 
@@ -20,13 +22,55 @@ class PassportBatch:
                 passport_fields[k] = v
             self.passports.append(Passport(passport_fields))
 
-    def valid_passports(self, allow_missing_fields=None):
+    def valid_passports(self, allow_missing_fields=None, strict=False):
         allow_missing_fields = allow_missing_fields or []
-        return list(filter(lambda p: p.is_valid(allow_missing_fields), self.passports))
+        return list(
+            filter(
+                lambda p: p.is_valid(
+                    allow_missing_fields=allow_missing_fields, strict=strict
+                ),
+                self.passports,
+            )
+        )
 
 
 class Passport:
     FIELDS = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"]
+
+    def height_validator(hgt):
+        m = re.fullmatch(r"(\d{2,3})(cm|in)", hgt)
+        if not m:
+            return False
+
+        scalar, units = m.groups()
+        scalar = int(scalar)
+
+        if units == "cm":
+            if scalar < 150 or scalar > 193:
+                return False
+        elif units == "in":
+            if scalar < 59 or scalar > 76:
+                return False
+        else:  # bad units
+            return False
+
+        return True
+
+    def year_validator(year, low, high):
+        return (
+            len(year) == 4 and year.isdigit() and int(year) >= low and int(year) <= high
+        )
+
+    STRICT_VALIDATORS = {
+        "byr": partial(year_validator, low=1920, high=2002),
+        "iyr": partial(year_validator, low=2010, high=2020),
+        "eyr": partial(year_validator, low=2020, high=2030),
+        "hgt": height_validator,
+        "hcl": lambda s: bool(re.fullmatch(r"#[0-9a-f]{6}", s)),
+        "ecl": lambda s: s in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"],
+        "pid": lambda s: len(s) == 9 and s.isdigit(),
+        "cid": lambda s: True,
+    }
 
     def __init__(self, raw):
         for k in raw:
@@ -35,12 +79,18 @@ class Passport:
             else:
                 raise ValueError(f"Unexpected field [{k}]")
 
-    def is_valid(self, allow_missing_fields=None):
+    def is_valid(self, allow_missing_fields=None, strict=False):
         allow_missing_fields = allow_missing_fields or []
+
         for f in self.FIELDS:
-            if not getattr(self, f, None) and f not in allow_missing_fields:
-                logging.debug("Missing attribute {f}")
+            fv = getattr(self, f, None)
+            if not fv and f not in allow_missing_fields:
+                logging.debug("Missing attribute %s", f)
                 return False
+            if strict:
+                if not self.STRICT_VALIDATORS[f](fv):
+                    logging.debug("Strict validation failed: %s=%s", f, fv)
+                    return False
 
         return True
 
@@ -48,4 +98,9 @@ class Passport:
 if __name__ == "__main__":
     puzzle = Puzzle(year=2020, day=4)
     batch = puzzle.input_data
-    puzzle.answer_a = len(PassportBatch(batch).valid_passports(["cid"]))
+    puzzle.answer_a = len(
+        PassportBatch(batch).valid_passports(allow_missing_fields=["cid"])
+    )
+    puzzle.answer_b = len(
+        PassportBatch(batch).valid_passports(allow_missing_fields=["cid"], strict=True)
+    )
--- adventofcode/year2020/day4/tests/test_solution.py
+++ adventofcode/year2020/day4/tests/test_solution.py
@@ -22,9 +22,69 @@ iyr:2011 ecl:brn hgt:59in
 """
 
 
+@pytest.fixture
+def invalid_batch():
+    return """
+eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+"""
+
+
+@pytest.fixture
+def valid_batch():
+    return """
+pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+"""
+
+
 def test_passport():
     with pytest.raises(ValueError):
         Passport({"foo": "bar"})
+
+
+@pytest.mark.parametrize(
+    "field,value,is_valid",
+    [
+        ("byr", "2002", True),
+        ("byr", "2003", False),
+        ("hgt", "60in", True),
+        ("hgt", "150cm", True),
+        ("hgt", "190cm", True),
+        ("hgt", "190in", False),
+        ("hgt", "190", False),
+        ("hcl", "#123abc", True),
+        ("hcl", "#123abz", False),
+        ("hcl", "123abc", False),
+        ("ecl", "brn", True),
+        ("ecl", "wat", False),
+        ("pid", "000000001", True),
+        ("pid", "0123456789", False),
+    ],
+)
+def test_strict_rules(field, value, is_valid):
+    assert Passport.STRICT_VALIDATORS[field](value) == is_valid
 
 
 def test_passport_batch(batch):
@@ -34,4 +94,16 @@ def test_passport_batch(batch):
     assert pb.passports[2].ecl == "brn"
 
     assert len(pb.valid_passports()) == 1
-    assert len(pb.valid_passports(["cid"])) == 2
+    assert len(pb.valid_passports(allow_missing_fields=["cid"])) == 2
+
+
+def test_strict_invalid_batch(invalid_batch):
+    pb = PassportBatch(invalid_batch)
+    assert len(pb.passports) == 4
+    assert len(pb.valid_passports(allow_missing_fields=["cid"], strict=True)) == 0
+
+
+def test_strict_valid_batch(valid_batch):
+    pb = PassportBatch(valid_batch)
+    assert len(pb.passports) == 4
+    assert len(pb.valid_passports(allow_missing_fields=["cid"], strict=True)) == 4
