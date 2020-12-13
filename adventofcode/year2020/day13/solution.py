235bb000d00745384af1d11211b2272b23508579
Ken Crowell <oeuftete@gmail.com>
2020-12-13 11:32:30 -0400

    wip: way too slow

--- adventofcode/year2020/day13/solution.py
+++ adventofcode/year2020/day13/solution.py
@@ -1,13 +1,16 @@
 import itertools
+import logging
 import math
 from dataclasses import dataclass, field
 
 from aocd.models import Puzzle
 
+logging.basicConfig(level=logging.INFO)
+
 
 @dataclass
 class BusNotes:
-    raw_notes: str
+    raw_notes: str = ""
     timestamp: int = 0
     bus_ids: list = field(default_factory=list)
 
@@ -15,19 +18,53 @@ class BusNotes:
         if self.raw_notes:
             lines = self.raw_notes.strip().splitlines()
             self.timestamp = int(lines.pop(0))
-            self.bus_ids = [int(n) for n in lines.pop(0).split(",") if n != "x"]
+            self.bus_ids = lines.pop(0).split(",")
 
     @property
     def waiting_product(self):
+        valid_ids = [int(id) for id in self.bus_ids if id.isdigit()]
         for t in itertools.count(start=self.timestamp):
-            for bus_id in self.bus_ids:
+            for bus_id in valid_ids:
                 if t % bus_id == 0:
                     return (t - self.timestamp) * bus_id
 
         return math.inf
+
+    @property
+    def offset_timestamp(self):
+        bus_map = {
+            int(id): int(offset)
+            for offset, id in enumerate(self.bus_ids)
+            if id.isdigit()
+        }
+        id_offset_zero = int(self.bus_ids[0])
+        max_id = max(bus_map)
+        logging.debug("Searching offset0=%s, max=%s", id_offset_zero, max_id)
+
+        matching_offset = math.inf
+        for t in itertools.count(step=id_offset_zero):
+            if t > 1_100_000:
+                break
+            logging.debug("Checking time %s", t)
+
+            try:
+                for bus_id, offset in bus_map.items():
+                    id_t = t + offset
+                    logging.debug("Checking for id %s at %s", bus_id, id_t)
+                    if id_t not in range(0, t + bus_id, bus_id):
+                        raise StopIteration(f"Time {t} does not match.")
+            except StopIteration as e:
+                logging.debug("Continuing: %s", e)
+                continue
+
+            matching_offset = t
+            break
+
+        return matching_offset
 
 
 if __name__ == "__main__":
     puzzle = Puzzle(year=2020, day=13)
     bus_notes = BusNotes(puzzle.input_data)
     puzzle.answer_a = bus_notes.waiting_product
+    puzzle.answer_b = bus_notes.offset_timestamp
--- adventofcode/year2020/day13/tests/test_solution.py
+++ adventofcode/year2020/day13/tests/test_solution.py
@@ -14,6 +14,22 @@ def fixture_raw_notes():
 def test_bus_notes(simple_raw_notes):
     bus_notes = BusNotes(simple_raw_notes)
     assert bus_notes.timestamp == 939
-    assert len(bus_notes.bus_ids) == 5
-    assert 59 in bus_notes.bus_ids
+    assert len(bus_notes.bus_ids) == 8
+    assert "59" in bus_notes.bus_ids
     assert bus_notes.waiting_product == 295
+
+
+@pytest.mark.parametrize(
+    "bus_ids,offset_timestamp",
+    [
+        ("7,13,x,x,59,x,31,19", 1068781),
+        # ("17,x,13,19", 3417),
+        # ("67,7,59,61", 754018),
+        # ("67,x,7,59,61", 779210),
+        # ("67,7,x,59,61", 1261476),
+        # ("1789,37,47,1889", 1202161486),
+    ],
+)
+def test_offset_timestamp(bus_ids, offset_timestamp):
+    bus_notes = BusNotes(bus_ids=bus_ids.split(","))
+    assert bus_notes.offset_timestamp == offset_timestamp
