7367278b540e6d43989d5afdf7e0ae6a74e7ed1b
Ken Crowell <oeuftete@gmail.com>
2020-12-14 18:50:30 -0400

    Add solution for 2020 day 14 b

--- adventofcode/year2020/day14/solution.py
+++ adventofcode/year2020/day14/solution.py
@@ -19,23 +19,46 @@ def empty_bitarray(n=36):
 @dataclass
 class SeaportComputer:
     instructions: list
-    or_mask: ba = field(default_factory=empty_bitarray)
-    and_mask: ba = field(default_factory=empty_bitarray)
+    version: int = 1
+    mask: str = "0" * 36
     memory: dict = field(default_factory=lambda: defaultdict(empty_bitarray))
+
+    def memset_v1(self, address, value):
+        and_mask = ba(self.mask.replace("X", "1"))
+        or_mask = ba(self.mask.replace("X", "0"))
+
+        self.memory[address] = int2ba(int(value), length=36) & and_mask | or_mask
+
+    def memset_v2(self, address, value):
+        #  All 0's don't change, so just ignore those.
+        #  All 1's set to 1.
+        one_or_mask = ba(self.mask.replace("X", "0"))
+        base_address = int2ba(int(address), length=36) | one_or_mask
+
+        #  All X's are "floating"
+        all_addresses = [base_address]
+        x_indices = [i for i, b in enumerate(self.mask, start=1) if b == "X"]
+        for xi in x_indices:
+            for a in all_addresses.copy():
+                all_addresses.append(a ^ int2ba(1 << (36 - xi), length=36))
+
+        for a in all_addresses:
+            self.memory[ba2int(a)] = int2ba(int(value), length=36)
 
     def process(self):
         while self.instructions:
             instruction = self.instructions.pop(0)
             action, value = instruction.split(" = ")
             if action == "mask":
-                self.or_mask = ba(value.replace("X", "0"))
-                self.and_mask = ba(value.replace("X", "1"))
+                self.mask = value
             elif action.startswith("mem"):
                 match = re.match(r"mem\[(\d+)\]", action)
                 address = int(match.groups()[0])
-                self.memory[address] = (
-                    int2ba(int(value), length=36) & self.and_mask | self.or_mask
-                )
+                if self.version == 1:
+                    self.memset_v1(address, value)
+                else:
+                    self.memset_v2(address, value)
+
             logging.debug("Memory contents: %s", self.memory)
 
     def sum_memory(self):
@@ -45,7 +68,12 @@ class SeaportComputer:
 if __name__ == "__main__":
     puzzle = Puzzle(year=2020, day=14)
     instructions = puzzle.input_data.strip().splitlines()
-    computer = SeaportComputer(instructions)
+    computer = SeaportComputer(instructions.copy())
     computer.process()
 
     puzzle.answer_a = computer.sum_memory()
+
+    v2_computer = SeaportComputer(instructions, version=2)
+    v2_computer.process()
+
+    puzzle.answer_b = v2_computer.sum_memory()
--- adventofcode/year2020/day14/tests/test_solution.py
+++ adventofcode/year2020/day14/tests/test_solution.py
@@ -1,5 +1,5 @@
 import pytest
-from bitarray.util import ba2int, int2ba
+from bitarray.util import ba2int
 
 from adventofcode.year2020.day14.solution import SeaportComputer
 
@@ -14,6 +14,16 @@ mem[8] = 0
 """.strip().splitlines()
 
 
+@pytest.fixture(name="v2_program")
+def fixture_v2_program():
+    return """
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
+""".strip().splitlines()
+
+
 def test_simple_program(simple_program):
     computer = SeaportComputer(simple_program)
     computer.process()
@@ -22,3 +32,10 @@ def test_simple_program(simple_program):
     assert ba2int(computer.memory[8]) == 64
     assert ba2int(computer.memory[9]) == 0
     assert computer.sum_memory() == 165
+
+
+def test_v2_program(v2_program):
+    computer = SeaportComputer(v2_program, version=2)
+    computer.process()
+
+    assert computer.sum_memory() == 208
