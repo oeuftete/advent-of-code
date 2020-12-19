acb55d5a62956df3f364d43754e90e1c269ad661
Ken Crowell <oeuftete@gmail.com>
2020-12-19 09:05:25 -0400

    Add solution for 2020 day 18 b

--- adventofcode/year2020/day18/solution.py
+++ adventofcode/year2020/day18/solution.py
@@ -30,12 +30,15 @@ class Equation:
         self.operator = o
 
     def resolve(self):
-        result = (
-            self.left * self.right if self.operator == "*" else self.left + self.right
-        )
-        self.left = result
-        self.operator = self.right = None
-        return result
+        if self.operator:
+            result = (
+                self.left * self.right
+                if self.operator == "*"
+                else self.left + self.right
+            )
+            self.left = result
+            self.operator = self.right = None
+        return self.left
 
 
 @dataclass
@@ -44,34 +47,96 @@ class Calculator:
 
     @property
     def tokenized(self):
-        return [c for c in self.equation if c != ""]
+        return [c for c in self.equation if c != " "]
 
     @property
     def result(self) -> int:
         calculation = [Equation()]
         for token in self.tokenized:
             logging.debug("[%s] Adding token [%s]", len(calculation), token)
+            current_eq = calculation[-1]
             if token == "(":
                 calculation.append(Equation())
+                current_eq = calculation[-1]
             elif token.isdigit():
-                calculation[-1].add_digit(token)
+                current_eq.add_digit(token)
             elif token in ("*", "+"):
-                calculation[-1].add_operator(token)
+                current_eq.add_operator(token)
             elif token == ")":
                 deep_result = calculation.pop().left
-                calculation[-1].add_digit(deep_result)
+                current_eq = calculation[-1]
+                current_eq.add_digit(deep_result)
 
-            if calculation[-1].is_complete:
-                interim = calculation[-1].resolve()
+            if current_eq.is_complete:
+                interim = current_eq.resolve()
                 logging.debug(
                     "[%s] Calculation was complete = %s", len(calculation), interim
                 )
             logging.debug("[%s] Resulting calcs: %s", len(calculation), calculation)
 
-        return calculation[-1].left
+        logging.debug("[%s] ### Final equation: %s", len(calculation), current_eq)
+        return current_eq.left
+
+    @property
+    def advanced_result(self) -> int:
+        calculation = [Equation()]
+
+        token_iter = iter(self.tokenized)
+        for token in token_iter:
+            logging.debug("[%s] Adding token [%s]", len(calculation), token)
+            current_eq = calculation[-1]
+            if token == "(":
+                #  Recursively get the result from subequation
+                sub_tokens = ""
+                paren_level = 0
+                while True:
+                    next_token = next(token_iter)
+                    if next_token == "(":
+                        paren_level += 1
+                    elif next_token == ")":
+                        if paren_level == 0:
+                            break
+                        paren_level -= 1
+                    sub_tokens += next_token
+
+                sub_result = Calculator(sub_tokens).advanced_result
+                logging.debug("[%s] SUBRESULT: [%s]", len(calculation), sub_result)
+
+                if current_eq.operator == "*":
+                    calculation.append(Equation())
+                    current_eq = calculation[-1]
+                current_eq.add_digit(sub_result)
+            elif token.isdigit():
+                #  If currently multiplying, start a new level
+                if current_eq.operator == "*":
+                    calculation.append(Equation())
+                    current_eq = calculation[-1]
+                current_eq.add_digit(token)
+            elif token in ("*", "+"):
+                current_eq.add_operator(token)
+
+            if current_eq.is_complete:
+                interim = current_eq.resolve()
+                logging.debug(
+                    "[%s] Calculation was complete = %s", len(calculation), interim
+                )
+            logging.debug("[%s] Resulting calcs: %s", len(calculation), calculation)
+
+        #  Resolve pending at the end
+        while len(calculation) > 1:
+            deep_result = calculation.pop().left
+            current_eq = calculation[-1]
+            current_eq.add_digit(deep_result)
+            current_eq.resolve()
+
+        logging.debug("[%s] ### Final equation: %s", len(calculation), current_eq)
+        return current_eq.left
 
 
 if __name__ == "__main__":
     puzzle = Puzzle(year=2020, day=18)
     homework = puzzle.input_data.strip().splitlines()
     puzzle.answer_a = sum([Calculator(equation).result for equation in homework])
+    puzzle.answer_b = sum(
+        [Calculator(equation).advanced_result for equation in homework]
+    )
--- adventofcode/year2020/day18/tests/test_solution.py
+++ adventofcode/year2020/day18/tests/test_solution.py
@@ -4,15 +4,19 @@ from adventofcode.year2020.day18.solution import Calculator
 
 
 @pytest.mark.parametrize(
-    "equation,result",
+    "equation,result,advanced_result",
     [
-        ("1 + 2 * 3 + 4 * 5 + 6", 71),
-        ("1 + (2 * 3) + (4 * (5 + 6))", 51),
-        ("2 * 3 + (4 * 5)", 26),
-        ("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437),
-        ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240),
-        ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632),
+        ("1 + 2 * 3 + 4 * 5 + 6", 71, 231),
+        ("1 + (2 * 3) + (4 * (5 + 6))", 51, 51),
+        ("2 * 3 + (4 * 5)", 26, 46),
+        ("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437, 1445),
+        ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240, 669060),
+        ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632, 23340),
+        ("8 * 8 * 9", 8 * 8 * 9, 8 * 8 * 9),
+        ("((4 * 2) * 2 + 2 + 6) + 6", 30, 86),
+        ("((7 * 9 * 5) + 9 * (8 + 2)) + 2", 3242, 3242),
     ],
 )
-def test_calculator(equation, result):
+def test_calculator(equation, result, advanced_result):
     assert Calculator(equation).result == result
+    assert Calculator(equation).advanced_result == advanced_result
