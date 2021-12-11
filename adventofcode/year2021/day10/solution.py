13914aed91001759a613808a70633e5b246eaf4b
Ken Crowell <oeuftete@gmail.com>
2021-12-11 00:01:13 -0400

    Add year 2021, day 10, part 2 solution

--- adventofcode/year2021/day10/solution.py
+++ adventofcode/year2021/day10/solution.py
@@ -1,5 +1,9 @@
+import logging
+
 import attr
 from aocd.models import Puzzle  # type: ignore
+
+LOGGER = logging.getLogger("solveaocd")
 
 
 @attr.s
@@ -7,27 +11,38 @@ class ParserLine:
     line: str = attr.ib()
     is_corrupt: bool = attr.ib(init=False, default=False)
     first_illegal: str = attr.ib(init=False, default="")
+    is_incomplete: bool = attr.ib(init=False, default=False)
+    autocompletion: str = attr.ib(init=False, default="")
 
     def __attrs_post_init__(self):
-        OPENER_MAP = {
+        PAIR_MAP = {}
+        for k, v in {
             ")": "(",
             "]": "[",
             "}": "{",
             ">": "<",
-        }
+        }.items():
+            PAIR_MAP[k] = v
+            PAIR_MAP[v] = k
 
         stack = []
         for c in self.line:
             if c in "([{<":
                 stack.append(c)
             if c in ")]}>":
-                if stack[-1] == OPENER_MAP[c]:
+                if stack[-1] == PAIR_MAP[c]:
                     stack.pop()
                 else:
                     self.first_illegal = c
                     self.is_corrupt = True
-
                     return
+
+        if not stack:
+            return
+
+        self.is_incomplete = True
+        while stack:
+            self.autocompletion += PAIR_MAP[stack.pop()]
 
 
 @attr.s
@@ -53,8 +68,41 @@ class Parser:
     def corrupt_lines(self) -> list[ParserLine]:
         return list(filter(lambda l: l.is_corrupt, self.parsed_lines))
 
+    @property
+    def incomplete_lines(self) -> list[ParserLine]:
+        return list(filter(lambda l: l.is_incomplete, self.parsed_lines))
+
+    @staticmethod
+    def autocomplete_score(s: str) -> int:
+        score = 0
+        for c in s:
+            match (c):
+                case (")"):
+                    score = 5 * score + 1
+                case ("]"):
+                    score = 5 * score + 2
+                case ("}"):
+                    score = 5 * score + 3
+                case (">"):
+                    score = 5 * score + 4
+
+            LOGGER.debug("Interim score for -%s-: %d", s, score)
+        return score
+
+    @property
+    def middle_score(self) -> int:
+        completion_scores = list(
+            map(
+                lambda l: self.autocomplete_score(l.autocompletion),
+                self.incomplete_lines,
+            )
+        )
+        LOGGER.debug(completion_scores)
+        return sorted(completion_scores)[len(completion_scores) // 2]
+
 
 if __name__ == "__main__":
     puzzle = Puzzle(year=2021, day=10)
     lines = puzzle.input_data.strip().splitlines()
     puzzle.answer_a = Parser(lines).syntax_score
+    puzzle.answer_b = Parser(lines).middle_score
--- adventofcode/year2021/day10/tests/test_solution.py
+++ adventofcode/year2021/day10/tests/test_solution.py
@@ -25,16 +25,21 @@ def test_example_lines(example_lines):
     assert len(p.corrupt_lines) == 5
     assert p.syntax_score == 26397
 
+    assert len(p.incomplete_lines) == 5
+    assert p.middle_score == 288957
+
 
 @pytest.mark.parametrize(
-    "line,is_corrupt,first_illegal",
+    "line,is_corrupt,is_incomplete,first_illegal,autocompletion",
     [
-        ("{([(<{}[<>[]}>{[]{[(<()>", True, "}"),
-        ("[({(<(())[]>[[{[]{<()<>>", False, ""),
+        ("{([(<{}[<>[]}>{[]{[(<()>", True, False, "}", ""),
+        ("[({(<(())[]>[[{[]{<()<>>", False, True, "", "}}]])})]"),
     ],
 )
-def test_parser_line(line, is_corrupt, first_illegal):
+def test_parser_line(line, is_corrupt, is_incomplete, first_illegal, autocompletion):
     pl = ParserLine(line)
 
     assert pl.is_corrupt == is_corrupt
+    assert pl.is_incomplete == is_incomplete
     assert pl.first_illegal == first_illegal
+    assert pl.autocompletion == autocompletion
