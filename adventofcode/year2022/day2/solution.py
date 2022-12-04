c1bb8412eeb5326374d3133128a250d4ccff47c3
Ken Crowell <oeuftete@gmail.com>
2022-12-04 11:44:55 -0400

    Solve year 2022, day 2, part b

--- adventofcode/year2022/day2/solution.py
+++ adventofcode/year2022/day2/solution.py
@@ -1,14 +1,20 @@
-from enum import Enum
+from enum import IntEnum
 
 import attr
 from aocd.models import Puzzle
 from cached_property import cached_property
 
 
-class RPS(Enum):
+class RPS(IntEnum):
     ROCK = A = X = 1
     PAPER = B = Y = 2
     SCISSORS = C = Z = 3
+
+
+class RpsResult(IntEnum):
+    X = LOSS = -1
+    Y = DRAW = 0
+    Z = WIN = 1
 
 
 @attr.s
@@ -36,23 +42,54 @@ class Game:
 @attr.s
 class StrategyGuide:
     guide_text: str = attr.ib()
+    real_scoring: bool = attr.ib(default=False)
     game_scores: list[int] = attr.ib(factory=list, init=False)
 
-    def __attrs_post_init__(self) -> None:
+    def game_score(self, rps_result: RpsResult, rps_me: RPS) -> int:
+        return 3 * (rps_result + 1) + rps_me
+
+    def _generate_real_scores(self):
+        for game in self.guide_text.splitlines():
+            (p_them, required_result) = game.split()
+            rps_them = RPS[p_them]
+            rps_required_result = RpsResult[required_result]
+
+            rps_required_me = (rps_them + rps_required_result) % 3
+            if rps_required_me == 0:
+                rps_required_me = 3
+
+            self.game_scores.append(
+                self.game_score(rps_required_result, rps_required_me)
+            )
+
+    def _generate_naive_scores(self):
         for game in self.guide_text.splitlines():
             (p_them, p_me) = game.split()
             rps_them = RPS[p_them]
             rps_me = RPS[p_me]
             self.game_scores.append(
-                3 * (Game(rps_me, rps_them).result + 1) + rps_me.value
+                self.game_score(Game(rps_me, rps_them).result, rps_me)
             )
 
-    @cached_property
+    def generate_scores(self):
+        self.game_scores = []
+        if self.real_scoring:
+            self._generate_real_scores()
+        else:
+            self._generate_naive_scores()
+
+    @property
     def total_score(self) -> int:
         return sum(self.game_scores)
 
 
 if __name__ == "__main__":
     puzzle = Puzzle(year=2022, day=2)
+
     strategy_guide = StrategyGuide(puzzle.input_data.strip())
+    strategy_guide.generate_scores()
     puzzle.answer_a = strategy_guide.total_score
+
+    strategy_guide.real_scoring = True
+    strategy_guide.generate_scores()
+    puzzle.answer_b = strategy_guide.total_score
--- adventofcode/year2022/day2/tests/test_solution.py
+++ adventofcode/year2022/day2/tests/test_solution.py
@@ -12,9 +12,17 @@ C Z
 """.strip()
 
 
-def test_example_lines(example_lines):
+def test_strategy_guide(example_lines):
     strategy_guide = StrategyGuide(example_lines)
 
+    strategy_guide.generate_scores()
     assert len(strategy_guide.game_scores) == 3
     assert strategy_guide.game_scores == [8, 1, 6]
     assert strategy_guide.total_score == 15
+
+    strategy_guide.real_scoring = True
+    strategy_guide.generate_scores()
+
+    assert len(strategy_guide.game_scores) == 3
+    assert strategy_guide.game_scores == [4, 1, 7]
+    assert strategy_guide.total_score == 12
