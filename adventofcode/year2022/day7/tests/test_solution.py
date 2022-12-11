2fcc5d2e70fd8f7f41fbcd10d8a100b2d806a1ef
Ken Crowell <oeuftete@gmail.com>
2022-12-10 21:06:43 -0400

    Solve year 2022, day 7, part b

--- adventofcode/year2022/day7/tests/test_solution.py
+++ adventofcode/year2022/day7/tests/test_solution.py
@@ -35,9 +35,17 @@ $ ls
 def test_example_commands(example_commands):
     dir_reader = DirReader(example_commands)
 
-    #  assert dir_reader.dir_size("/") == 48381165
-    #  assert dir_reader.dir_size("a") == 94853
-    #  assert dir_reader.dir_size("d") == 24933642
-    #  assert dir_reader.dir_size("e") == 584
+    assert dir_reader.dir_size(dir_reader.root) == 48381165
+    assert dir_reader.dir_size(dir_reader.find_inode(["a"])) == 94853
+    assert dir_reader.dir_size(dir_reader.find_inode(["a", "e"])) == 584
+    assert dir_reader.dir_size(dir_reader.find_inode(["d"])) == 24933642
+
+    assert dir_reader.node_dir_sizes() == [
+        584,
+        94853,
+        24933642,
+        48381165,
+    ]
 
     assert dir_reader.part_a_solution == 95437
+    assert dir_reader.part_b_solution == 24933642
