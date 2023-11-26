076cd81a4c11e8bc0909ad8fb9446e4c14d131a4
Ken Crowell <oeuftete@gmail.com>
2023-11-26 13:37:48 -0400

    Use aocd example for a few tests

--- adventofcode/year2022/day1/tests/test_solution.py
+++ adventofcode/year2022/day1/tests/test_solution.py
@@ -1,26 +1,14 @@
 import pytest
+from aocd.models import Puzzle
 
 from adventofcode.year2022.day1.solution import CalorieCounter
+
+EXAMPLE = Puzzle(year=2022, day=1).examples[0]
 
 
 @pytest.fixture(name="example_lines")
 def fixture_example_lines():
-    return """
-1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000
-""".strip()
+    return EXAMPLE.input_data
 
 
 def test_example_lines(example_lines):
@@ -28,5 +16,5 @@ def test_example_lines(example_lines):
 
     assert len(calorie_counter.calories) == 5
     assert calorie_counter.calories[0] == 6000
-    assert calorie_counter.max_calories == 24000
-    assert calorie_counter.top_three_calories == 45000
+    assert calorie_counter.max_calories == int(EXAMPLE.answer_a)
+    assert calorie_counter.top_three_calories == int(EXAMPLE.answer_b)
--- adventofcode/year2022/day7/tests/test_solution.py
+++ adventofcode/year2022/day7/tests/test_solution.py
@@ -1,39 +1,18 @@
 import pytest
+from aocd.models import Puzzle
 
 from adventofcode.year2022.day7.solution import DirReader
+
+EXAMPLE = Puzzle(year=2022, day=7).examples[0]
 
 
 @pytest.fixture(name="example_commands")
 def fixture_example_commands():
-    return """
-$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k
-""".strip().splitlines()
+    return EXAMPLE.input_data
 
 
 def test_example_commands(example_commands):
-    dir_reader = DirReader(example_commands)
+    dir_reader = DirReader(example_commands.splitlines())
 
     assert dir_reader.dir_size(dir_reader.root) == 48381165
     assert dir_reader.dir_size(dir_reader.find_inode(["a"])) == 94853
@@ -47,5 +26,5 @@ def test_example_commands(example_commands):
         48381165,
     ]
 
-    assert dir_reader.part_a_solution == 95437
-    assert dir_reader.part_b_solution == 24933642
+    assert dir_reader.part_a_solution == int(EXAMPLE.answer_a)
+    assert dir_reader.part_b_solution == int(EXAMPLE.answer_b)
